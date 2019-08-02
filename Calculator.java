5920a50dad567b431572ca09756fc701222ae561
Omar Faruqe <faruqe.cse@gmail.com>
2019-08-02 18:14:01 +0600

    subtraction method called from main

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@ public class Calculator{
 	public static void main(String[] args){
 
 		System.out.println(addition(3,4));
-
+		System.out.println(subtraction(13,4));
 	}
 
 	double addition(double x, int double){
